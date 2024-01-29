0653d3da9776b8a2053f2e0f7b2588d3a8672366
MANOJ S <95956060+jst-manu@users.noreply.github.com>
2024-01-29 11:30:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+let boxes=document.querySelectorAll(".box");
+let playAgain=document.querySelector("#playAgain");
+let reset=document.querySelector("#reset");
+let message=document.querySelector(".message");
+let msg=document.querySelector("#msg");
+let f=document.querySelector("#game");
+let newGame=document.querySelector("#new");
+let player1=document.querySelector("#p1");
+let player2=document.querySelector("#p2");
+let count=0;
+let turnO=true;
+const winpt=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+const resetGame=()=>{
+    turnO=true;
+    enableBoxes();
+    message.classList.add("hide");
+}
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if(turnO){
+            box.innerText='O';
+            turnO=false;
+        }
+        else{
+            box.innerText='X';
+            turnO=true;
+        }
+        box.disabled=true;
+        winner();    
+    });
+});
+
+const disableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+
+    }
+}
+const enableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+
+    }
+}
+
+const showWinner=(win)=>{
+    msg.innerText=`Congratulations Winner is '${win}'`;
+    reset.disabled=true;
+    message.classList.remove("hide");
+    disableBoxes();
+      win==='O'?p1.innerText=count+1:p2.innerText=count+1;
+    
+    
+};
+
+const winner=()=>{
+    for(let pattern of winpt){
+        let pos1=boxes[pattern[0]].innerText;
+        let pos2=boxes[pattern[1]].innerText;
+        let pos3=boxes[pattern[2]].innerText;
+        if(pos1 !="" && pos2 !="" && pos3 !=""){
+            if(pos1===pos2 && pos2===pos3){
+                showWinner(pos1);
+                
+            }
+            
+        }
+
+    }
+
+};
+
+reset.addEventListener("click",resetGame);
+playAgain.addEventListener("click",resetGame);
+newGame.addEventListener("click",()=>{
+    p1.innerText=count;
+    p2.innerText=count;
+});
+newGame.addEventListener("click",resetGame);
+
